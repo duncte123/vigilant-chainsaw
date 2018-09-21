264d135caf47efa2e39a2191640f6db611968507
Duncan Sterken <duncan.sterken123@gmail.com>
2018-09-21 14:07:43 +0200

    Use the new fetch method

--- js/main.js
+++ js/main.js
@@ -1,81 +1,72 @@
 let token = "";
 
-function submitForm() {
-    //clear the messages paragraph
-    _("messages").innerHTML = "Sending.....";
+function submitForm () {
+  //clear the messages paragraph
+  _("messages").innerHTML = "Sending.....";
 
-    if(token == "") {
-        _("messages").innerHTML = "Please complete the captcha.";
-        return;
-    }
+  if (token === "") {
+    _("messages").innerHTML = "Please complete the captcha.";
+    return;
+  }
 
-    //Get all the things
-    let fname = _("fname").value,
-        lname = _("lname").value,
-        email = _("email").value,
-        website = _("website").value,
-        message = _("message").value;
+  //Get all the things
+  let fname = _("fname").value,
+      lname = _("lname").value,
+      email = _("email").value,
+      website = _("website").value,
+      message = _("message").value;
 
-    // noinspection EqualityComparisonWithCoercionJS
-    let flag = fname == "" || lname == "" || email == "" || message == "";
 
-    if(flag) {
-        _("messages").innerHTML = "Please fill out all required fields";
-        return;
-    }
+  let flag = fname === "" || lname === "" || email === "" || message === "";
 
-    //Replace all characters that we don't want in our db
-    fname = fname.replace(/[^0-9a-z]/gi, "");
-    lname = lname.replace(/[^0-9a-z]/gi, "");
-    email = email.replace(/[^0-9a-z.@]/gi, "");
-    message = message.replace(/[^0-9a-z ]/gi, "");
+  if (flag) {
+    _("messages").innerHTML = "Please fill out all required fields";
+    return;
+  }
 
-    let data = new FormData();
-    data.append("fname", fname);
-    data.append("lname", lname);
-    data.append("email", email);
-    data.append("message", message);
-    data.append("g-recaptcha-response", token);
+  //Replace all characters that we don't want in our db
+  fname = fname.replace(/[^0-9a-z]/gi, "");
+  lname = lname.replace(/[^0-9a-z]/gi, "");
+  email = email.replace(/[^0-9a-z.@]/gi, "");
+  message = message.replace(/[^0-9a-z ]/gi, "");
 
-    // noinspection EqualityComparisonWithCoercionJS
-    if(website != "") {
-        website = website.replace(/[^a-z0-9\/\\:+*\-_.]/gi, "");
-        data.append("website", website);
-    }
+  let data = new FormData();
+  data.append("fname", fname);
+  data.append("lname", lname);
+  data.append("email", email);
+  data.append("message", message);
+  data.append("g-recaptcha-response", token);
 
-    const ajax = new XMLHttpRequest();
-    ajax.open("POST", "/parsers/form_parser.php");
-    ajax.onreadystatechange = ev => {
-        console.log(ev);
-        // These comments are here just to make my ide stop complaining
-        // noinspection EqualityComparisonWithCoercionJS
-        if(ajax.readyState == 4 && ajax.status == 200 ) {
-            // noinspection EqualityComparisonWithCoercionJS
-            if(ajax.responseText == "success") {
-                _("cont").innerHTML = "<h4 class='center'>Thanks for your message</h4>"
-            } else {
-                _("messages").innerHTML = "Something went wrong: " + ajax.responseText;
-            }
+  // noinspection EqualityComparisonWithCoercionJS
+  if (website != "") {
+    website = website.replace(/[^a-z0-9\/\\:+*\-_.]/gi, "");
+    data.append("website", website);
+  }
+
+  fetch("/parsers/form_parser.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: data
+  }).then(r => r.text())
+      .then((text) => {
+        if (text === "success") {
+          _("cont").innerHTML = "<h4 class='center'>Thanks for your message</h4>"
         } else {
-            _("messages").innerHTML = "Something went wrong, try again later.";
+          _("messages").innerHTML = "Something went wrong: " + text;
         }
-    };
-
-    ajax.send(data);
+      });
 }
 
-function getToken(t) {
-    token = t;
-}
-
-function _(el) { return document.getElementById(el); }
+const _ =  (el) => document.getElementById(el);
 
 /**
  * This function turns every * in the form element red.
  * Just because ICBA to do it with html
  */
 $(document).ready(() => {
-    let form = _("form");
-    let form_content = form.innerHTML;
-    form.innerHTML = form_content.replace(/\*/g, "<span class=\"star\">*</span>");
+  let form = _("form");
+  let form_content = form.innerHTML;
+  form.innerHTML = form_content.replace(/\*/g, "<span class=\"star\">*</span>");
 });
--- parsers/form_parser.php
+++ parsers/form_parser.php
@@ -1,5 +1,6 @@
-<?php
-if( isset($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], $_POST["g-recaptcha-response"]) &&
+<?php /** @noinspection SqlNoDataSourceInspection, SqlDialectInspection */
+
+if (isset($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], $_POST["g-recaptcha-response"]) &&
     !isEmpty($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], $_POST["g-recaptcha-response"])) {
 
     $curl = curl_init();
@@ -12,14 +13,14 @@ if( isset($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], 
             // https://developers.google.com/recaptcha/docs/faq#id-like-to-run-automated-tests-with-recaptcha-v2-what-should-i-do
             'secret' => "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe",
             'response' => $_POST["g-recaptcha-response"],
-            "remoteip" => getRealIpAddr()
+            "remoteip" => $_SERVER["HTTP_CF_CONNECTING_IP"],
         ],
     ]);
 
     $chapRes = json_decode(curl_exec($curl));
     curl_close($curl);
 
-    if($chapRes->success != true){
+    if ($chapRes->success != true) {
         die("Captcha check failed, please try again later");
     }
 
@@ -32,13 +33,13 @@ if( isset($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], 
         "lname" => $_POST["lname"],
         "email" => $_POST["email"],
         "message" => $_POST["message"],
-        "ip" => getRealIpAddr()
+        "ip" => $_SERVER["HTTP_CF_CONNECTING_IP"],
     ];
 
     $sql = "INSERT INTO guestbook VALUES(default, :fname , :lname, :email, NULL , :message, :ip , default)";
 
-    if(isset($_POST["website"]) && !empty($_POST["website"])) {
-        $sqlFields += [ "website" => $_POST["website"] ];
+    if (isset($_POST["website"]) && !empty($_POST["website"])) {
+        $sqlFields += ["website" => $_POST["website"]];
         $sql = "INSERT INTO guestbook VALUES(default, :fname , :lname , :email , :website , :message, :ip , default)";
     }
 
@@ -52,47 +53,26 @@ if( isset($_POST["fname"], $_POST["lname"], $_POST["email"], $_POST["message"], 
         } else {
             die("Entry could not be saved in the database");
         }
-    }
-    catch (PDOException $e) {
+    } catch (PDOException $e) {
         die($e->getMessage());
     }
-
-
 } else {
     die("Some required fields where not set or where empty.");
 }
 
 /**
  * @param array ...$items the items to check
+ *
  * @return bool true if one the items is empty
  */
-function isEmpty(...$items) {
+function isEmpty(...$items)
+{
 
     foreach ($items as $item) {
-        if(empty($item)) {
+        if (empty($item)) {
             return true;
         }
     }
 
     return false;
-}
-
-/**
- * http://itman.in/en/how-to-get-client-ip-address-in-php/
- */
-function getRealIpAddr()
-{
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-    {
-        $ip=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-    {
-        $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else
-    {
-        $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
 }
